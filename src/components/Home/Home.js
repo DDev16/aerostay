20026131bb4d1e71ea0a624037158fdd5aece2e1
DDev16 <dillondevera12@gmail.com>
2023-08-29 02:10:12 -0700

    push

---
+++ src/components/Home/Home.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './Home.css'; // Import your CSS file for styling
+
+const categories = [
+  "Cabins",
+  "Amazing views",
+  "Lakefront",
+  "Treehouses",
+  "Windmills",
+  "Mansions",
+  "Beachfront",
+  "OMG!",
+  "Amazing pools",
+  "Tiny homes",
+  "Trending",
+  "Countryside",
+  "Off-the-grid",
+  "Domes",
+  "Play",
+  "Farms",
+  "Boats",
+  "Islands",
+  "Castles",
+  "Luxe",
+  "Houseboats",
+  "A-frames",
+  "Camping",
+  "National parks",
+  "Design",
+  "Barns",
+  "Caves",
+  "Vineyards",
+  "Containers",
+  "Tropical",
+  "New",
+  "Skiing",
+  "Bed & breakfasts",
+  "Creative spaces",
+  "Earth homes",
+  "Rooms",
+  "Historical homes",
+  "Top of the world",
+  "Ski-in/out",
+  "Golfing",
+  "Iconic cities",
+  "Yurts",
+  "Arctic",
+  "Adapted",
+  "Chef's kitchens",
+  "Surfing",
+  "Desert",
+  "Towers",
+  "Minsus",
+  "Cycladic homes",
+  "Casas particulares",
+  "Grand pianos",
+  "Riads",
+  "Ryokans",
+  "Trulli",
+  "Hanoks",
+  "Shepherd's huts",
+  "Dammusi",
+  "Campers",
+  "Beach",
+  "Lake"
+];
+
+const listings = [
+    // Sample listing objects, you would fetch real data from the blockchain or a database
+    { title: "Cozy Cabin", category: "Cabins" },
+    { title: "Panoramic View House", category: "Amazing views" },
+    { title: "Lakefront Retreat", category: "Lakefront" },
+    // Add more listing objects here
+  ];
+
+  
+  function Home() {
+    
+  }
+  
+  export default Home;
